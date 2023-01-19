fed27e29467cb41f7b08394fa4c7907990f6a5fa
atabong <tajoacha@gmail.com>
2023-01-19 20:33:42 +0300

    Home Adjustment
    Made some adjustments on the home page code so the that it has the /Home tag on the link tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ window.addEventListener('scroll',changeColor)
        <Link to='/'><h1>GLX TRVL</h1></Link>
        <ul className={click? 'nav-menu active': 'nav-menu'}>
             <li>
-                <Link to='/'>Home</Link>
+                <Link to='/Home'>Home</Link>
             </li>
             <li>
                 <Link to='/pricing'>Pricing</Link>
